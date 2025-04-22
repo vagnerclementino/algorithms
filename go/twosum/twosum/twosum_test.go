7eb974afae558f85d4c2db5b276d09fdd2a467cf
Vagner Clementino <vagner.clementino@hotmart.com>
2025-04-22 17:02:19 -0300

    test: Refactor TestTwoSum to use table-driven tests with parallel execution

--- go/twosum/twosum/twosum_test.go
+++ go/twosum/twosum/twosum_test.go
@@ -7,26 +7,75 @@ import (
 	"github.com/vagnerclementino/algorithms/go/twosum/twosum"
 )
 
-func TestTwoSum(t *testing.T) {
-	tests := []struct {
-		nums   []int
-		target int
-		want   []int
-	}{
-		{[]int{2, 7, 11, 15}, 9, []int{0, 1}},
-		{[]int{3, 2, 4}, 6, []int{1, 2}},
-		{[]int{3, 3}, 6, []int{0, 1}},
-		{[]int{1, 2, 3, 4}, 7, []int{2, 3}},
+func TestTwoSum_Calculate(t *testing.T) {
+
+	t.Parallel()
+	tests := map[string]func(t *testing.T){
+		"when receive a valid input with nums {2, 7, 11, 15} and target 9": func(t *testing.T) {
+
+			t.Helper()
+			// arrange
+			nums := []int{2, 7, 11, 15}
+			target := 9
+
+			// act
+			got := twosum.Calculate(nums, target)
+
+			// assert
+			if !reflect.DeepEqual(got, []int{0, 1}) {
+				t.Errorf("Expected %v, but got %v", []int{0, 1}, got)
+			}
+		},
+		"when receive a valid input with nums {3, 2, 4} and target 6": func(t *testing.T) {
+			t.Helper()
+			// arrange
+			nums := []int{3, 2, 4}
+			target := 6
+
+			// act
+			got := twosum.Calculate(nums, target)
+
+			// assert
+			if !reflect.DeepEqual(got, []int{1, 2}) {
+				t.Errorf("Expected %v, but got %v", []int{1, 2}, got)
+			}
+		},
+		"when receive a valid input with nums {3, 3} and target 6": func(t *testing.T) {
+			t.Helper()
+			// arrange
+			nums := []int{3, 3}
+			target := 6
+
+			// act
+			got := twosum.Calculate(nums, target)
+
+			// assert
+			if !reflect.DeepEqual(got, []int{0, 1}) {
+				t.Errorf("Expected %v, but got %v", []int{0, 1}, got)
+			}
+		},
+		"when receive a valid input with nums {1, 2, 3, 4} and target 7": func(t *testing.T) {
+			t.Helper()
+			// arrange
+			nums := []int{1, 2, 3, 4}
+			target := 7
+
+			// act
+			got := twosum.Calculate(nums, target)
+
+			// assert
+			if !reflect.DeepEqual(got, []int{2, 3}) {
+				t.Errorf("Expected %v, but got %v", []int{2, 3}, got)
+			}
+		},
 	}
 
-	for _, tt := range tests {
-		tt := tt // capture range variable
-		t.Run("", func(t *testing.T) {
+	for name, run := range tests {
+		run := run
+
+		t.Run(name, func(t *testing.T) {
 			t.Parallel()
-			got := twosum.Calculate(tt.nums, tt.target)
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("twoSum(%v, %d) = %v; want %v", tt.nums, tt.target, got, tt.want)
-			}
+			run(t)
 		})
 	}
 }
