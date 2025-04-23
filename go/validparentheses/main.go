1ec513a7f3b35ef52533cf930d4672ea7e6ea541
Vagner Clementino <vagner.clementino@hotmart.com>
2025-04-23 10:37:51 -0300

    feat: Implement stack data structure and add validation tests for parentheses

--- go/validparentheses/main.go
+++ go/validparentheses/main.go
@@ -2,6 +2,37 @@ package main
 
 import "fmt"
 
+type stack struct {
+	data []rune
+}
+
+func NewStack() *stack {
+	return &stack{
+		data: []rune{},
+	}
+}
+
+func (s *stack) Push(r rune) {
+	s.data = append(s.data, r)
+}
+
+func (s *stack) Pop() rune {
+	r := s.data[len(s.data)-1]
+	s.data = s.data[:len(s.data)-1]
+	return r
+}
+
+func (s *stack) Peek() rune {
+	if len(s.data) == 0 {
+		return '*'
+	}
+	return s.data[len(s.data)-1]
+}
+
+func (s *stack) Size() int {
+	return len(s.data)
+}
+
 func isOpenPar(r rune) bool {
 	switch r {
 	case '{':
@@ -12,7 +43,6 @@ func isOpenPar(r rune) bool {
 		return true
 	default:
 		return false
-
 	}
 }
 
@@ -26,9 +56,7 @@ func getClosePar(r rune) rune {
 		return ']'
 	default:
 		return '*'
-
 	}
-
 }
 func isValid(s string) bool {
 	if len(s) == 0 {
@@ -36,19 +64,15 @@ func isValid(s string) bool {
 	} else if len(s) == 1 {
 		return false
 	}
-	stack := []rune{}
-	stackSize := 0
-
+	stack := NewStack()
 	for _, r := range s {
 		if isOpenPar(r) {
-			stack = append(stack, r)
-			stackSize++
-		} else if r == getClosePar(stack[stackSize-1]) {
-			stack = stack[:stackSize-1]
-			stackSize--
+			stack.Push(r)
+		} else if r == getClosePar(stack.Peek()) {
+			stack.Pop()
 		}
 	}
-	return stackSize == 0
+	return stack.Size() == 0
 }
 
 func main() {
